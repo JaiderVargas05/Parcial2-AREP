1b656673eb639d17fe350b0e9e064bdfb53787b8
unknown <jaider.vargas-n@labinfo.is.escuelaing.edu.co>
2025-10-22 09:01:46 -0500

    initial commit

---
+++ MathServices/src/main/java/co/edu/escuelaing/mathservices/MathServices.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package co.edu.escuelaing.mathservices;
+
+/**
+ *
+ * @author jaider.vargas-n
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathServices {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MathServices.class, args);
+    }
+
+}
