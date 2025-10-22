54244987e4158a899c05a56258d26bc867e8cae5
unknown <jaider.vargas-n@labinfo.is.escuelaing.edu.co>
2025-10-22 09:45:37 -0500

    proxy service creation

---
+++ ProxyServices/src/main/java/co/edu/escuelaing/proxyservices/ProxyServices.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package co.edu.escuelaing.proxyservices;
+
+/**
+ *
+ * @author jaider.vargas-n
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyServices {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProxyServices.class, args);
+    }
+
+}
